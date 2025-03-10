8c2ff8081248a3a406cdc9d697e054530d9448f1
RozanovIgor <rozanovil@gmail.com>
2025-03-10 16:28:11 +0300

    new version - create portfolio for user and monkey

--- sandbox_add_user.py
+++ sandbox_add_user.py
@@ -1,76 +1,9 @@
-# print("hello world")
-import requests
-import psycopg2
-
-
-TOKEN = "CG-dDR9LwhXkWppyaUki3YEWVxb"
-url = f"https://api.coingecko.com/api/v3/coins/markets?x_cg_demo_api_key={TOKEN}"
-
-headers = {"accept": "application/json"}
-params = {
-    "vs_currency": "usd",
-    "per_page": 30
-}
-
-# response = requests.get(url, headers=headers, params=params)
-# data = response.text
-# with open("coins_markets.json", mode='w', encoding="utf-8") as f:
-#     f.write(data)
-
-connection = psycopg2.connect(
-        host = 'localhost',
-        user = 'postgres',
-        password = 'password',
-        dbname = 'postgres',
-        port = 5432
-)
-
-with connection.cursor() as cursor:
-        
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS monkey_users (
-            monkey_user_id SERIAL PRIMARY KEY,
-            monkey_user_login TEXT NOT NULL)
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS user_portfolio (
-            user_portfolio_id serial primary key,
-            monkey_user_id INTEGER,
-            FOREIGN KEY (monkey_user_id) REFERENCES monkey_users(monkey_user_id)                           
-            )
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS user_portfolio_details (
-            user_portfolio_details_id serial primary key,
-            user_portfolio_id INTEGER,
-            symbol TEXT NOT NULL,
-            quantity FLOAT,
-            FOREIGN KEY (user_portfolio_id) REFERENCES user_portfolio(user_portfolio_id)                           
-            )
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS monkey_portfolio_details (
-            monkey_portfolio_details_id serial primary key,
-            user_portfolio_id INTEGER,
-            symbol TEXT NOT NULL,
-            quantity FLOAT,
-            FOREIGN KEY (user_portfolio_id) REFERENCES user_portfolio(user_portfolio_id)                           
-            )
-        """)        
-      
-connection.commit()
-
-
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 import json
 from tkinter.messagebox import showerror, showwarning, showinfo
 import psycopg2
-import random
 # from config import host,user, password, dbname, port
 import locale
 import sys
@@ -78,7 +11,6 @@ import sys
 current_user = {}
 current_user_id = {}
 new_portfolio = []
-current_portfolio_id = {}
 
 class Database:
     def __init__(self):
@@ -123,14 +55,12 @@ class Database:
         global new_portfolio
         global current_user
         global current_user_id
-        global current_portfolio_id
         print(current_user_id)
         cursor.execute('INSERT INTO user_portfolio (monkey_user_id) VALUES (%s)',(current_user_id,)) #добавляем новый порфтель в таблицу перечня клиентских портфелей
         self.connection.commit()
         cursor.execute('SELECT MAX(user_portfolio_id) FROM user_portfolio WHERE monkey_user_id = %s',(current_user_id,)) #достаем id для добавленного портфеля 
         a = cursor.fetchall()
         new_portfolio_id = int(a[0][0]) #преобразовываем полученные данные в цифру
-        current_portfolio_id = new_portfolio_id
         # print(new_portfolio_id)
         load = []
         for a in new_portfolio:
@@ -146,61 +76,9 @@ class Database:
         cursor.executemany('INSERT INTO user_portfolio_details (user_portfolio_id, symbol, quantity) VALUES (%s,%s,%s)',(load)) #добавляем данные в новый порфтель в таблицу перечня клиентских портфелей
         self.connection.commit()
 
-    def get_current_portfolio_currency_count(self):
-        global current_portfolio_id
-        cursor = self.connection.cursor()
-        cursor.execute('SELECT COUNT(user_portfolio_id) FROM user_portfolio_details WHERE user_portfolio_id = %s',(current_portfolio_id,)) #достаем id для добавленного портфеля 
-        current_portfolio_currency_count  = int(cursor.fetchall()[0][0])
-        return(current_portfolio_currency_count)
-            # print(random.randint(0,current_portfolio_currency_count))
-
-    def get_currency_count(self):
-            cursor = self.connection.cursor()
-            cursor.execute('SELECT COUNT(symbol) FROM currency_prices') #достаем количество валют из справочника
-            currency_count = int(cursor.fetchall()[0][0])
-            return(currency_count)
-
-    def get_currency_list(self):
-        cursor = self.connection.cursor()
-        cursor.execute('SELECT (symbol) FROM currency_prices') #достаем список валют из справочника
-        currency_list = cursor.fetchall()
-        return(currency_list)
-        
-    def add_monkey_portfolio(self,monkey_portfolio_currency_list):
-        cursor = self.connection.cursor()
-        cursor.executemany("""INSERT INTO monkey_portfolio_details (symbol,user_portfolio_id,quantity)
-        VALUES (%s,%s,%s)""",(monkey_portfolio_currency_list)) #достаем список валют из справочника
-        self.connection.commit()
-
-    def get_user_statistics(self):
-        global current_user_id
-        cursor = self.connection.cursor()
-        cursor.execute("""SELECT (symbol) FROM monkey WHERE monkey_user_id = %s""",(current_user_id)) #выбрать все позиции портфелей клиента по его id - много SELECT?
-
-
+    
 
 database = Database()
-
-def generate_monkey_portfolio(current_portfolio_currency_count,currency_count):
-    global current_portfolio_id
-    a = int(0)
-    monkey_portfolio_list = []
-    current_portfolio_currency_count = int(current_portfolio_currency_count)
-    currency_count = int(currency_count)
-    while a < current_portfolio_currency_count:
-            monkey_currency = random.randint(0,currency_count)
-            if monkey_portfolio_list.count(monkey_currency) < 1:
-                monkey_portfolio_list.append(monkey_currency)
-            else: a = a - 1
-            a = a + 1
-    print(monkey_portfolio_list) 
-    currency_list = database.get_currency_list()
-    monkey_portfolio_currency_list = []
-    for a in monkey_portfolio_list:
-        monkey_portfolio_currency = currency_list[int(a)]
-        monkey_portfolio_currency = monkey_portfolio_currency + (int(current_portfolio_id),) + (int(1),) 
-        monkey_portfolio_currency_list.append(monkey_portfolio_currency)
-    return(monkey_portfolio_currency_list)
 
 root = Tk()
 root.title("Вход")
@@ -260,7 +138,7 @@ def portfolio_create_screen():
             self.list = tk.Listbox(self)
             self.list.insert(0, *DAYS)
             self.print_btn = tk.Button(self, text="Создать портфель",
-                                    command=self.create_user_and_monkey_portfolios)
+                                    command=self.print_selection)
             self.btns = [self.create_btn(m) for m in MODES]
 
             self.list.pack()
@@ -273,16 +151,11 @@ def portfolio_create_screen():
             return tk.Button(self, command=cmd,
                             text=mode.capitalize())
 
-        def create_user_and_monkey_portfolios(self):
+        def print_selection(self):
             selection = self.list.curselection()
             global new_portfolio
-            global current_portfolio_id
             new_portfolio = [self.list.get(i) for i in selection]
             database.create_portfolio()
-            monkey_portfolio_currency_list = generate_monkey_portfolio(database.get_current_portfolio_currency_count(),database.get_currency_count())
-            print(monkey_portfolio_currency_list)
-            database.add_monkey_portfolio(monkey_portfolio_currency_list)
-            switch_frame(user_screen)
 
 
     if __name__ == "__main__":
@@ -291,12 +164,4 @@ def portfolio_create_screen():
 
 switch_frame(login_screen)
 
-root.mainloop()    
-
-
-
-
-
-
-
-
+root.mainloop()     
